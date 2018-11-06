827507157709aeac0b04421f9628d391dd69c564
Fatema Ferdousy <panna123@yahoo.com>
2018-11-06 23:39:23 +0100

    just test

---
+++ NewProject/src/Game/MonsterTest.java
@@ -0,0 +1,14 @@
+package Game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MonsterTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
