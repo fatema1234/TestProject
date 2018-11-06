d561f9f78177020fd2d103965bbde193dbcd1a08
Fatema Ferdousy <panna123@yahoo.com>
2018-11-06 21:15:07 +0100

    just commit

---
+++ NewProject/src/Game/gamePlayer.java
@@ -0,0 +1,92 @@
+package Game;
+
+public class gamePlayer {
+
+	
+	private static int skill;
+	private static int attribute;
+	private int position;
+	private int weapon;
+	private int life;
+	 
+	
+	public gamePlayer(int skill, int attribute, int position, int weapon, int life) {
+        
+		this.skill = skill;
+		this.attribute = attribute;
+		this.position = position;
+		this.weapon = weapon;
+		this.life = life;
+		
+}
+	
+	public gamePlayer() {
+	}
+
+	public void initialize(int s,int a) {
+		
+		int x = 0;
+		gamePlayer.skill = x;
+		gamePlayer.attribute = 100;
+		
+		
+	}
+	
+	public void fightDragon() {
+		
+		boolean win = true;
+		if(win) {setPlayerAttributeSkill:
+			return;
+		}else {
+			return;
+			
+		}
+		
+	
+	 Object Direction;
+	Object Distance;
+	moveTo(Direction,Distance);{
+			
+			Object distance;
+			int Direct;
+			int terrain;
+			switch(Direct):
+				case left:
+					if(canMove(terrain,play.posh))
+						updatePlayerPosition(distance,direction);
+					
+		}
+	
+	 canMove(terrain,position,distance);{
+		return(terrain-position)>=distance;
+		
+	}
+	
+	
+	Object direct;
+	updatePlayerPosition(distance,direct);{
+		this.position = position;
+	}
+	
+	getAsset(assetId);{
+		Object remove;
+		((Object) remove).AssetList();
+		increasePlayerSkill();
+	
+	}
+
+	private void updatePlayerPosition(Object distance, Object direct) {
+		
+		
+	}
+
+	private void increasePlayerSkill() {
+		
+		
+	}
+
+}
+	
+	
+
+
---
+++ NewProject/src/Game/gamePlayerTest.java
@@ -0,0 +1,14 @@
+package Game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class gamePlayerTest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
