57ccb748e2436e742ff4a636b70a2d1ff92ff298
Fatema Ferdousy <panna123@yahoo.com>
2018-11-07 01:06:40 +0100

    only monsterTest

--- NewProject/src/Game/MonsterTest.java
+++ NewProject/src/Game/MonsterTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 
 class MonsterTest {
 
-	Monster mon = new Monster(); 
-	
+	Monster mon = new Monster(0, null, 0); 
+	gamePlayer gp = new gamePlayer();
 	@Test
 	void testGetMaxStrengthStrong() {
 		assertEquals(200, mon.getMaxStrength());
@@ -18,13 +18,23 @@ class MonsterTest {
 	}
 	@Test 
 	void testGetMaxStrengthWeak() {
-		mon = new Monster(); 
+		mon = new Monster(0, null, 0); 
 		mon.getDownEnergy();
 		int[] expected = {50, 0};
 		int[] actual = {mon.getMaxStrength(), mon.getskill()};
 		assertArrayEquals(expected, actual);
 	}
 	
-	
+@Test		
+  void gettAttack() {
+		
+            mon.increaseSkill();
+			mon.getAttack();
+			gp[]expected = {mon,gp};
+			assertArrayEquals(expected, mon.getAttack());
+		}
+@Test
+void getRandomAttack() {
 	
 }
+}		
