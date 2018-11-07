e57b5c1400044c822adc9b049f5d883ff487c1bb
Fatema Ferdousy <panna123@yahoo.com>
2018-11-07 01:15:01 +0100

    1

--- NewProject/src/Game/MonsterTest.java
+++ NewProject/src/Game/MonsterTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 class MonsterTest {
 
@@ -37,4 +38,9 @@ class MonsterTest {
 void getRandomAttack() {
 	
 }
+@Test
+void getMonsterWinn() {
+			
+}
+
 }		
